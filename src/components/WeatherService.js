dce0331044d71351edf85e2179eb93383af30f95
Jackson Fuller <112270926+jaxfuller@users.noreply.github.com>
2023-12-11 17:41:01 -0700

    hid API key

--- src/components/WeatherService.js
+++ src/components/WeatherService.js
@@ -1,4 +1,4 @@
-const apiKey = '3fc96fec7169f2cd4904ac43ac2ddb40';
+const apiKey = '[HIDDEN]';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 
 const fetchWeatherData = async (destination) => {
